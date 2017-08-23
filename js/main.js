f3bff9c43006bb9967de577f6b27a04ded8c10be
terentyevo <oleg-t@live.ru>
2017-08-23 18:28:30 +0300

    форма обратной связи, меню

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//owl carousel
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
       responsive: {
@@ -14,3 +15,29 @@ $(document).ready(function(){
       verticalCentered: false
     });
 });
+
+//team menu
+function teamAcco(){
+  $('.team-accordeon').on('click', function(e){
+      e.preventDefault()
+    
+      var elem = $(e.target),
+          item = elem.parent();
+
+      items = item.siblings();
+
+      if(!item.hasClass('active')){
+          item.addClass('active');
+          items.removeClass('active');
+
+      }
+      else{
+          item.removeClass('active');
+      }    
+
+      
+    
+    
+  })
+};
+teamAcco();
